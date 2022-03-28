eda34b66facc01563d6d2f5aa78bd53c99d31664
Benjamin Soltoff <ben@bensoltoff.com>
2022-03-28 11:28:44 -0500

    Incorporate elements from Weiss's class for day 1 slides. Clean up slide formatting

---
+++ R/slide-opts.R
@@ -0,0 +1,14 @@
+options(htmltools.dir.version = FALSE)
+knitr::opts_chunk$set(
+  cache = TRUE,
+  echo = TRUE,
+  message = FALSE,
+  warning = FALSE,
+  fig.width = 8, fig.asp = 0.618, fig.retina = 2, dpi = 150,
+  fig.showtext = TRUE,
+  fig.align = "center",
+  out.width = "80%"
+)
+
+# set base theme
+ggplot2::theme_set(ggplot2::theme_minimal(base_size = rcfss::base_size))
